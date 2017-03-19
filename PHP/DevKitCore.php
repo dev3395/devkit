882005ce05cd2cf276d1dab085863ab45486d79a
dev1d <dimitri@Kvorka.local>
2017-03-19 17:06:52 -0400

    Style Guide integration with template
    and other cleanup

--- PHP/DevKitCore.php
+++ PHP/DevKitCore.php
@@ -2,60 +2,60 @@
 
 function readConfig()
 {
-	global $config_log_file;
-	global $logfile;
-	
-	// Read local config file
-	$configString = file_get_contents(CONFIG_FILE_NAME, true);
-	//print $configString;
-	
-	//Split the string and take just first line. Otherwise $key could include a line break could cause problems for the variable
-	//$line1 = strtok($configString, "\n");
-	//print $line1;
+    global $config_log_file;
+    global $logfile;
+    
+    // Read local config file
+    $configString = file_get_contents(CONFIG_FILE_NAME, true);
+    //print $configString;
+    
+    //Split the string and take just first line. Otherwise $key could include a line break could cause problems for the variable
+    //$line1 = strtok($configString, "\n");
+    //print $line1;
 
-	//For JSON files
-	//print "\nConfig values: \n";
-	$parsed_json = json_decode($configString);
-	
-	$config_log_file = $parsed_json->{'system_config'}->{'log_file'};
-	//print $config_log_file . "\n\n\n";
-	
-	$logfile = $config_log_file;
-	print "Logging configured. See messages in: $logfile\n";
+    //For JSON files
+    //print "\nConfig values: \n";
+    $parsed_json = json_decode($configString);
+    
+    $config_log_file = $parsed_json->{'system_config'}->{'log_file'};
+    //print $config_log_file . "\n\n\n";
+    
+    $logfile = $config_log_file;
+    print "Logging configured. See messages in: $logfile\n";
 
-	print "Reminder -- new output is probably in the DEBUG LOG FILE *** : $logfile\n";
-	$msg= "----------------------\nStarting logging in $logfile";
-	logToFile($logfile, $msg);
-	
-	$config_computer_name = $parsed_json->{'system_config'}->{'computer_name'};
-	//logToFile($logfile, $config_computer_name);
+    print "Reminder -- new output is probably in the DEBUG LOG FILE *** : $logfile\n";
+    $msg= "----------------------\nStarting logging in $logfile";
+    logToFile($logfile, $msg);
+    
+    $config_computer_name = $parsed_json->{'system_config'}->{'computer_name'};
+    //logToFile($logfile, $config_computer_name);
 
-	//User configuration
-	//$config_user_name = $parsed_json->{'user_config'}->{'user_name'};
-	//logToFile($logfile, $config_user_name);
+    //User configuration
+    //$config_user_name = $parsed_json->{'user_config'}->{'user_name'};
+    //logToFile($logfile, $config_user_name);
 }
 
 function logToFile($filename, $msg) 
 {  
-	$fd = fopen($filename, "a"); 
+    $fd = fopen($filename, "a"); 
 
-	date_default_timezone_set('UTC');
-	$str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;  
+    date_default_timezone_set('UTC');
+    $str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;  
 
-	fwrite($fd, $str . "\n"); 
-	fclose($fd); 
+    fwrite($fd, $str . "\n"); 
+    fclose($fd); 
 } 
-	
-	
-	function writeToFile($filename, $msg) 
-	{  
-		$fd = fopen($filename, "a"); 
+    
+    
+function writeToFile($filename, $msg) 
+{  
+    $fd = fopen($filename, "a"); 
 
-		date_default_timezone_set('UTC');
-		$str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;  
+    date_default_timezone_set('UTC');
+    $str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;  
 
-		fwrite($fd, $msg . "\n"); 
-		fclose($fd); 
-	} 
-		
+    fwrite($fd, $msg . "\n"); 
+    fclose($fd); 
+} 
+        
 ?>
--- PHP/DevKitTemplate.php
+++ PHP/DevKitTemplate.php
@@ -1,41 +1,83 @@
 <?php
 /*
-	HISTORY
-	StaticID: DX1RG20170313T1242s16
-	Purpose: PHP template
-	2017-03-13 12:42:34 : First version
 
-	INFO
-	To allow execution from command line, set first line to: #!/usr/bin/php
-	To call from command line: ./filename.php
-	Any text above the <?php line besides the #! line  will be included in the response, which you probabl don't want.
+
+# PURPOSE
+- PHP template
+- PHP basic framework with related core functions
+- Style guide
+
+# HISTORY:
+StaticID: DX1RG20170313T1242s16
+
+- 2017-03-13 12:42:34 : First version
+
+
+# TODO:
+- Add classes/methods
+- Improve parameter passing
+
+
+# STYLE GUIDE
+- Based on FIG standards https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-1-basic-coding-standard.md
+- Basics reflected in template below
+- Notes:
+    - 4 spaces, not tabs
+    - funtionNames
+    - variable_names
+    - CONSTANT_NAMES
+    
+- Control structure keywords MUST have one space after them; method and
+function calls MUST NOT.
+
+- Opening braces for control structures MUST go on the same line, and closing
+braces MUST go on the next line after the body.
+
+- Opening parentheses for control structures MUST NOT have a space after them,
+and closing parentheses for control structures MUST NOT have a space before.
+
+
+# TIPS
+- To allow execution from command line, set first line to: #!/usr/bin/php
+- To call from command line: ./filename.php
+- Any text above the <?php line besides the #! line  will be included in the response, which you probabl don't want.
+ 
 */
-	
+
+
 include 'DevKitCore.php';
 
-if ($argc > 1) {
-	print "Argument count: $argc\n";
-	print "Argument 1:     $argv[1]\n";
-	// var_dump($argv);
-	}
+// var_dump($argv);
+
 
 const CONFIG_FILE_NAME = "config.json";
 
 global $config_log_file;
 global $logfile;
 
-main();
+print "\nArgument count: $argc . \n";
 
-function main()
-{
-	global $logfile;
-	
-	print "Starting main loop\n";
-	
-	readConfig(CONFIG_FILE_NAME);
-	
-	logToFile($logfile, "Configuration file and logging enabled");
-	logToFile($logfile, "Continuing with main loop");
-		
+if ($argc >1) {
+    print "\nArgument 1:     $argv[1]\n";
+} else {
+    //
 }
+
+
+mainLoop();
+
+function mainLoop()
+{
+    global $logfile;
+    
+    print "Starting main loop\n";
+    
+    readConfig(CONFIG_FILE_NAME);
+    
+    logToFile($logfile, "Configuration file and logging enabled");
+    logToFile($logfile, "Continuing with main loop");
+}
+
+
+
 ?>
