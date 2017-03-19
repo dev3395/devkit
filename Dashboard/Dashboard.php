780e71f539b988a51cd77c230dca3f6e8df261d3
dev1m <dimitri@MBP16.local>
2017-03-19 17:51:52 -0400

    cleanup

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -24,16 +24,22 @@ const CONFIG_FILE_NAME = "config.json";
 
 global $config_log_file;
 global $logfile;
+
 const TIMER_INFO = "timer.txt";
 
 $param = $argv[1];
+
 main();
+
 //print $param;
+
 
 timer($param);
 
-function main()
-{
+uptime();
+
+
+function main() {
 	global $logfile;
 	
 	print "Starting main loop\n";
@@ -42,39 +48,43 @@ function main()
 	
 	logToFile($logfile, "Configuration file and logging enabled");
 	logToFile($logfile, "Continuing with main loop");
-}
+	}
 
 
 
-function uptime() {
-$uptime = shell_exec('uptime');
+function uptime()
+{
+	$uptime = shell_exec('uptime');
 	$pattern = "~\s[0-9]\sdays~";
 
-		$success = preg_match($pattern, $uptime, $match);
-		if ($success) {
-		echo "Uptime:" . $match[0] . "\n";
-			
-}
-}
-
-function timer($param){
-global $logfile;
-
-logToFile($logfile, $param);
-
-if ($param = "start") {
-	logToFile($logfile, "yes");
+	$success = preg_match($pattern, $uptime, $match);
+	if ($success) {
+	echo "Uptime:" . $match[0] . "\n";
+	}
 }
 
-		$start_time = time();
-		$max = 50000000;
-		for ($i = 1; $i <= $max; $i++) {
-		}
-		$y = time();
-		$difference = $y - $start_time;
 
-		//logToFile($logfile, $difference);
-		writeToFile(TIMER_INFO, $start_time);
-	
+
+function timer($param)
+{
+	global $logfile;
+
+	logToFile($logfile, $param);
+
+	if ($param = "start") {
+		logToFile($logfile, "yes");
+	}
+
+	$start_time = time();
+	$max = 50000000;
+	for ($i = 1; $i <= $max; $i++) {
+		//
+	}
+	$y = time();
+	$difference = $y - $start_time;
+
+	//logToFile($logfile, $difference);
+	writeToFile(TIMER_INFO, $start_time);
+		
 }
 ?>
