4683fef1d90767bd41ca719b0db89e2489b1f1d9
dev1m <dimitri@Argon.local>
2017-10-07 14:58:30 -0400

    CSS updates

--- PHP/DevKitTemplate.php
+++ PHP/DevKitTemplate.php
@@ -5,7 +5,6 @@
 # PURPOSE
 - PHP template
 - PHP basic framework with related core functions
-- Style guide
 
 # HISTORY:
 StaticID: DX1RG20170313T1242s16
@@ -18,35 +17,11 @@ StaticID: DX1RG20170313T1242s16
 - Improve parameter passing
 
 
-# STYLE GUIDE
-- Based on FIG standards https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-1-basic-coding-standard.md
-- Basics reflected in template below
-- Notes:
-    - 4 spaces, not tabs
-    - funtionNames
-    - variable_names
-    - CONSTANT_NAMES
-    
-- Control structure keywords MUST have one space after them; method and
-function calls MUST NOT.
-
-- Opening braces for control structures MUST go on the same line, and closing
-braces MUST go on the next line after the body.
-
-- Opening parentheses for control structures MUST NOT have a space after them,
-and closing parentheses for control structures MUST NOT have a space before.
-
-
-# TIPS
-- To allow execution from command line, set first line to: #!/usr/bin/php
-- To call from command line: ./filename.php
-- Any text above the <?php line besides the #! line  will be included in the response, which you probabl don't want.
- 
 */
 
 
 
-include 'DevKitCore.php';
+include 'devkit_core.php';
 
 // var_dump($argv);
 
